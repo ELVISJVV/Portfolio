f00a880b9302c491af19bebd7f739b6aa86b2810
ELVISJVV <elvisvasquez257@gmail.com>
2023-09-19 23:58:20 -0600

    intro right side

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,7 +3,12 @@ import './Intro.css'
 import Github from '../../img/github.png'
 import LinkedIn from '../../img/linkedin.png'
 import Instagram from '../../img/instagram.png'
-
+import Vector1 from '../../img/Vector1.png'
+import Vector2 from '../../img/Vector2.png'
+import boy from '../../img/boy.png'
+import Thumbup from '../../img/thumbup.png'
+import Crown from '../../img/crown.png'
+import Glassesimoji from '../../img/glassesimoji.png'
 const Intro = () => {
     return (
         <div className="intro">
@@ -15,12 +20,25 @@ const Intro = () => {
                 </div>
                 <button className="button i-button">Hire me</button>
                 <div className="i-icons">
+                <a href="">
+
                     <img src={Github} alt="" />
+                </a>
+                <a href="">
+
                     <img src={LinkedIn} alt="" />
+                </a>
+                <a href="">
+
                     <img src={Instagram} alt="" />
+                </a>
                 </div>
             </div>
-            <div className="i-right"></div>
+            <div className="i-right">
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={boy} alt="" />
+            </div>
         </div>
     )
 }
