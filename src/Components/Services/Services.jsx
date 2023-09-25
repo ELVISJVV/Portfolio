e663d6cc6d063673b4bf676f71ddd6a67a696516
ELVISJVV <elvisvasquez257@gmail.com>
2023-09-25 00:03:38 -0600

    create card component

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,24 +3,30 @@ import './Services.css'
 import HeartEmoji from '../../img/heartemoji.png'
 import Humble from '../../img/humble.png'
 import Glassesimoji from '../../img/glassesimoji.png'
+import Card from '../Card/Card'
 
 const Services = () => {
     return (
-        <div className="sevices">
+        <div className="services">
 
             <div className="awesome">
                 <span>My Awesome</span>
                 <span>services</span>
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo est a, cum officia quis. Reiciendis saepe mollitia illum quidem hic!
-                <br />
+                <span>Lorem ipsum dolor sit amet consectetug elit. Quo est a,  saepe mollitia illum quidem hic!
+                    <br />
                     quisquam saepe in ab vi. Reiciendis saepe mollitia illum quidem hic!
                 </span>
                 <button className="button s-button">Download CV</button>
-                <div className="blur s-blur" style={{background:"#abf1ff94"}}></div>
+                <div className="blur s-blur" style={{ background: "#abf1ff94" }}></div>
             </div>
 
             <div className="cards">
-                right side
+                <div>
+                    <Card
+                    emoji ={HeartEmoji}
+                    heading = {'Design'}
+                    detail = {"Figma, Sketch, Photoshpo,Adobe, Adobe xd "} ></Card>
+                </div>
             </div>
         </div>
     )
