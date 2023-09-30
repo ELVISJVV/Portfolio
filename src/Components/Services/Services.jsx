791102415fa52b637cbf695e7f7d7eeb19b5ef08
ELVISJVV <elvisvasquez257@gmail.com>
2023-09-29 23:58:21 -0600

    create second card

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Services.css'
 import HeartEmoji from '../../img/heartemoji.png'
 import Humble from '../../img/humble.png'
-import Glassesimoji from '../../img/glassesimoji.png'
+import Glasses from '../../img/glasses.png'
 import Card from '../Card/Card'
 
 const Services = () => {
@@ -21,11 +21,18 @@ const Services = () => {
             </div>
 
             <div className="cards">
-                <div>
+                <div style={{left:'14rem'}}>
                     <Card
                     emoji ={HeartEmoji}
                     heading = {'Design'}
                     detail = {"Figma, Sketch, Photoshpo,Adobe, Adobe xd "} ></Card>
+                </div>
+
+                <div style={{ top: '12rem' ,left: '-4rem' }}>
+                    <Card
+                        emoji={Glasses}
+                        heading={'Developer'}
+                        detail={"HTML, CSS, JavaScriot, React "} ></Card>
                 </div>
             </div>
         </div>
