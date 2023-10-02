ac3caca31cd58f2460e6a43712559e9bb6b9c38f
ELVISJVV <elvisvasquez257@gmail.com>
2023-10-02 00:12:20 -0600

    finish component portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -15,7 +15,11 @@ const Portfolio = () => {
             <span>Portfolio</span>
 
             {/* slider */}
-            <Swiper>
+            <Swiper
+            spaceBetween={30}
+            slidesPerView={3}
+            grabCursor={true}
+            className='portfolio-slider'>
                 <SwiperSlide>
                     <img src={Sidebar} alt="" />
                 </SwiperSlide>
