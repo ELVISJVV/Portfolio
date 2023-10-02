a2ab531ceaa2cf01ee8775eb8eafe98e2ee87132
ELVISJVV <elvisvasquez257@gmail.com>
2023-10-02 11:30:11 -0600

    finish component testimonials

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -42,7 +42,7 @@ const Testimonials = () => {
                 <span>Exceptional Work</span>
                 <span>from me...</span>
                 <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
-                <div className="blur t-blur2" style={{ background: "var(skyblue)" }}></div>
+                <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
             </div>
             {/* slider */}
             <Swiper modules={[Pagination]}
