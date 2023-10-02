b500110c98c7e2dca3c7c6218cf1321ed8e33069
ELVISJVV <elvisvasquez257@gmail.com>
2023-10-02 10:29:27 -0600

    create component testimonial

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Services from './Components/Services/Services'
 import Experience from './Components/Experience/Experience'
 import Works from './Components/Works/Works'
 import Portfolio from './Components/Portfolio/Portfolio'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
+      <Testimonials />
     </div>
   )
 }
