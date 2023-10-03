22bd23fd1073e49524c771e363e713f2698c8587
ELVISJVV <elvisvasquez257@gmail.com>
2023-10-02 23:51:27 -0600

    finish toggle

--- src/Components/Toggle/Toggle.jsx
+++ src/Components/Toggle/Toggle.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import './Toggle.css'
 import Sun from '@iconscout/react-unicons/icons/uil-sun'
 import Moon from '@iconscout/react-unicons/icons/uil-moon'
+import { themeContext } from '../../Context'
+import { useContext } from 'react'
 
 const Toggle = () => {
-  return (
-    <div className="toggle">
-        <Moon  />
-        <Sun  />
-        <div className="t-button">
+    const theme = useContext(themeContext)
+    const darkMode = theme.state.darkMode
 
+    const handleClick = () => {
+        theme.dispatch({ type: 'TOGGLE' })
+    }
+
+    return (
+        <div className="toggle"
+            onClick={handleClick}>
+            <Moon />
+            <Sun />
+            <div className="t-button"
+
+                style={darkMode ? { left: '2px' } : { right: '2px' }
+                }
+            >
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Toggle
