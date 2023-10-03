1ca8362f2e6267f750bac50cf3715d95c59d01b9
ELVISJVV <elvisvasquez257@gmail.com>
2023-10-03 12:09:17 -0600

    update navbar

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -15,7 +15,7 @@ const Services = () => {
     const darkMode = theme.state.darkMode;
 
     return (
-        <div className="services">
+        <div className="services" id='Services'>
 
             <div className="awesome">
                 <span style={{ color: darkMode ? 'white' : '' }} >My Awesome</span>
