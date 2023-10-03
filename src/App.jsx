5d8f67e2ba0b46974e1a3ae6cad5c79c72acf313
ELVISJVV <elvisvasquez257@gmail.com>
2023-10-02 21:21:07 -0600

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Works from './Components/Works/Works'
 import Portfolio from './Components/Portfolio/Portfolio'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
       <Portfolio />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   )
 }
