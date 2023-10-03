8e83ecf90dc6ae96ad6b9e20cb79058745484ef9
ELVISJVV <elvisvasquez257@gmail.com>
2023-10-03 11:28:07 -0600

    create animation

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -6,15 +6,19 @@ import Glasses from '../../img/glasses.png'
 import Card from '../Card/Card'
 import { themeContext } from '../../Context'
 import { useContext } from 'react'
+import { motion } from 'framer-motion'
 
 const Services = () => {
+
+    const transition = { duration: 1, type: 'spring' }
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
+
     return (
         <div className="services">
 
             <div className="awesome">
-                <span style = {{color: darkMode? 'white' : ''}} >My Awesome</span>
+                <span style={{ color: darkMode ? 'white' : '' }} >My Awesome</span>
                 <span>services</span>
                 <span>Lorem ipsum dolor sit amet consectetug elit. Quo est a,  saepe mollitia illum quidem hic!
                     <br />
@@ -27,28 +31,42 @@ const Services = () => {
                 <div className="blur s-blur1" style={{ background: "#abf1ff94" }}></div>
             </div>
 
-            <div className="cards">
-                <div style={{ left: '14rem' }}>
+            <div
+
+                className="cards">
+                <motion.div
+                    initial={{ left: "19rem" }}
+                    whileInView={{ left: "14rem" }}
+                    transition={transition}
+                    style={{ left: '14rem' }}>
                     <Card
                         emoji={HeartEmoji}
                         heading={'Design'}
                         detail={"Figma, Sketch, Photoshpo,Adobe, Adobe xd "} ></Card>
-                </div>
+                </motion.div>
 
-                <div style={{ top: '12rem', left: '-4rem' }}>
+                <motion.div
+                    initial={{ left: "-11rem",top:"12rem" }}
+                    whileInView={{ left: "-4rem" }}
+                    transition={transition}
+                    style={{ top: '12rem', left: '-4rem' }}>
                     <Card
                         emoji={Glasses}
                         heading={'Developer'}
                         detail={"HTML, CSS, JavaScriot, React "} ></Card>
-                </div>
+                </motion.div>
 
-                <div style={{ top: '19rem', left: '12rem' }}>
+                <motion.div
+                    initial={{ top: "19rem", left: "25rem" }}
+                    whileInView={{ left: "12rem" }}
+                    transition={transition}
+                    style={{ top: '19rem', left: '12rem' }}>
                     <Card
                         emoji={Humble}
                         heading={'UI/UX'}
                         detail={"loerem adadadaekdakd "} ></Card>
-                </div>
-                <div className="blur s-blur2" style={{background: "var(--purple)"}}></div>
+                </motion.div>
+                <div className="blur s-blur2" style={{ background: "var(--purple)" }}></div>
             </div>
         </div>
     )
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -7,9 +7,12 @@ import Shopify from '../../img/Shopify.png'
 import Facebook from '../../img/Facebook.png'
 import { themeContext } from '../../Context'
 import { useContext } from 'react'
+import {motion} from 'framer-motion'
 
 
 const Works = () => {
+
+
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
 
@@ -33,7 +36,12 @@ const Works = () => {
 
             {/* right side */}
             <div className="w-right">
-                <div className="w-mainCircle">
+                <motion.div
+                initial={{rotate: 45}}
+                whileInView={{rotate: 0}}
+                viewport={{margin:'-40px'}}
+                transition={{duration:3.5, type:'spring'}}
+                className="w-mainCircle">
                     <div className="w-secCircle">
                         <img src={Upwork} alt="" />
                     </div>
@@ -49,7 +57,7 @@ const Works = () => {
                     <div className="w-secCircle">
                         <img src={Facebook} alt="" />
                     </div>
-                </div>
+                </motion.div>
                 {/* background Circles */}
                 <div className="w-backCircle blueCircle"></div>
                 <div className="w-backCircle yellowCircle"></div>
